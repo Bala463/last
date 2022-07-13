f80971a62ee5651712eb34a8658e72e31675cd13
“BalaBalu” <balabalumohan@gmail.com>
2022-07-13 14:43:57 +0000

    1st commit

---
+++ last.java
@@ -0,0 +1,8 @@
+mport java.util.*;
+public class StrontioNumberExample1.
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.print("Enter the number: ");
+//reading an integer from the user.
